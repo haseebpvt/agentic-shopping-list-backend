23d070434831323a461dedf8e6b0f0c3893a5882
Haseeb <haseebpvt@gmail.com>
2025-08-25 23:36:28 +0530

    Refactor database connection parameters to use environment variables.

--- di/dependencies.py
+++ di/dependencies.py
@@ -8,14 +8,14 @@ load_dotenv()
 
 async def get_tidb_connection():
     connection = pymysql.connect(
-        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-        port=4000,
-        user=os.getenv("TIDB_USER"),
+        host=os.getenv("TIDB_HOST"),
+        port=int(os.getenv("TIDB_PORT")),
+        user=os.getenv("TIDB_USERNAME"),
         password=os.getenv("TIDB_PASSWORD"),
-        database="shopping",
-        ssl_verify_cert=True,
-        ssl_verify_identity=True,
-        ssl_ca="/etc/ssl/cert.pem"
+        database=os.getenv("TIDB_DATABASE"),
+        # ssl_verify_cert=True,
+        # ssl_verify_identity=True,
+        # ssl_ca="/etc/ssl/cert.pem"
     )
 
     return connection
