04145457a2c9c78c9ea40007b914296f2f175b28
Haseeb <haseebpvt@gmail.com>
2025-08-29 18:29:03 +0530

    Enhance search_vector_db function to include table and user_id parameters.

--- db/vector_db_search.py
+++ db/vector_db_search.py
@@ -1,8 +1,26 @@
-def search_vector_db(query: str, k: int = 5):
+from pytidb import Table
+
+
+def search_vector_db(
+        query: str,
+        table: Table,
+        user_id: int,
+        k: int = 5,
+):
     """
     Search and return k number of result form vector database.
+    :param user_id:
+    :param table: TiDB table reference
     :param query: The query text
     :param k: Number of result to return
     :return: List of string representing matching data
     """
-    return [f"Test query {i}" for i in range(1, 10)]
+
+    df = (
+        table.search(query)
+        .filter({"user_id": user_id})
+        .limit(k)
+        .to_list()
+    )
+
+    return [item["text"] for item in df]
