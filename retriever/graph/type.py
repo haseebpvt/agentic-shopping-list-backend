4dee45a783c0bfb5e10ff3fa4a06f44f147fd80f
haseebpvt <haseebpvt@gmail.com>
2025-08-30 00:02:37 +0530

    Changed type to BaseModel

--- retriever/graph/type.py
+++ retriever/graph/type.py
@@ -1,14 +1,15 @@
 import operator
-from typing import List, Annotated, TypedDict
+from typing import List, Annotated
+from pydantic import BaseModel
 
 
-class State(TypedDict):
+class State(BaseModel):
     queries: List[str]
     user_id: str
     results: Annotated[list, operator.add]
 
 
-class WorkerState(TypedDict):
+class WorkerState(BaseModel):
     query: str
     user_id: str
     results: Annotated[list, operator.add]
