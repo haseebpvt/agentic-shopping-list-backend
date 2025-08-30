7846dbdc65c596a264964f570401f75c35ec463b
haseebpvt <haseebpvt@gmail.com>
2025-08-30 18:21:15 +0530

    Add ProductList type to group multiple Product instances together

--- graph/nodes.py
+++ graph/nodes.py
@@ -1,6 +1,6 @@
 from langchain_core.messages import SystemMessage, HumanMessage
 
-from graph.type import State, SuggestedProduct, SuggestedProductList
+from graph.type import State, SuggestedProduct, SuggestedProductList, ProductList
 from llm.llm import get_llm
 from prompt.prompt_loader import get_prompt_template
 
@@ -38,7 +38,26 @@ def product_suggestion_node(state: State) -> SuggestedProduct:
 
 
 def describe_image_node(state: State):
-    pass
+    """Node for describing the products in the base64 image"""
+    llm = get_llm()
+
+    prompt = get_prompt_template("describe_image")
+
+    result = llm.invoke(
+        input=[
+            HumanMessage(content=[
+                {"type": "text", "text": prompt},
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{state.image_base64}"},
+                }
+            ]),
+        ]
+    )
+
+    product_list = llm.with_structured_output(schema=ProductList).invoke(input=result.content)
+
+    return {"product_items": product_list}
 
 
 def generate_prompts_node(state: State):
