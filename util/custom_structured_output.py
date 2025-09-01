a44ba74af498efaca89301abd9315da867af0fed
Haseeb <haseebpvt@gmail.com>
2025-09-01 15:59:40 +0530

    Refactor output handling in custom_structured_output for improved clarity.

--- util/custom_structured_output.py
+++ util/custom_structured_output.py
@@ -1,31 +1,14 @@
 from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel
-from pydantic.v1.main import ModelMetaclass
 
-from graph.type import Product
-from llm.llm import get_llm
-
-x = Product
-print(type(x))
 
 def custom_structured_output(
-        model: ModelMetaclass,
+        model: type[BaseModel],
         llm: BaseChatModel,
         prompt: str,
 ):
-    llm_with_structured_output = llm.with_structured_output(model)
-    chain = llm | llm_with_structured_output
+    parser = PydanticOutputParser(pydantic_object=model)
+
+    chain = llm | parser
     return chain.invoke(prompt)
-
-
-if __name__ == '__main__':
-    data = '''
-    Product list:
-    - Lux: Dishwasher
-    - Lays: The tasty chip
-    '''
-
-    result = custom_structured_output(Product, get_llm(), "Extract the producst")
-
-    print(result)
