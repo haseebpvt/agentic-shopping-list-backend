6a81724bd73e46c88dccdc55a26a3c089e251e71
haseebpvt <haseebpvt@gmail.com>
2025-09-02 11:06:12 +0530

    Add Quiz and QuestionAnswer models and include quiz in State model

--- graph/type.py
+++ graph/type.py
@@ -32,6 +32,16 @@ class EnoughPreferences(BaseModel):
     reason: str = Field(description="The reason of weather the preference is enough or not enough.")
     is_enough_preferences: bool
 
+
+class QuestionAnswer(BaseModel):
+    question: str
+    answers: List[str]
+
+
+class Quiz(BaseModel):
+    quiz: List[QuestionAnswer]
+
+
 class State(BaseModel):
     image_base64: str = ""
     user_id: str = ""
@@ -39,4 +49,5 @@ class State(BaseModel):
     queries: List[str] = []
     preference_vector_search_results: List[str] = []
     is_preferences_enough: EnoughPreferences | None = None
+    quiz: Quiz | None = None
     suggested_products: SuggestedProductList | None = None
