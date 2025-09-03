d5c3dfc25a4f0ddd40e3c38d7d0d38e75ee7e0cb
Haseeb <haseebpvt@gmail.com>
2025-09-03 17:25:23 +0530

    Add shopping and preference node to the graph builder function.

--- extractor/graph/builder.py
+++ extractor/graph/builder.py
@@ -1,7 +1,15 @@
 from langgraph.graph import StateGraph, START, END
 
+from extractor.graph.nodes import extract_shopping_and_preference_node
 from extractor.graph.type import State
 
 
 def build_graph():
     graph = StateGraph(State)
+
+    graph.add_node("extract_shopping_and_preference_node", extract_shopping_and_preference_node)
+
+    graph.add_edge(START, "extract_shopping_and_preference_node")
+    graph.add_edge("extract_shopping_and_preference_node", END)
+
+    return graph.compile()
