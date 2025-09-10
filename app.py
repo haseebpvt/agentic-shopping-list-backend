6740f7aef74f6163b4658ee3b4bb78103891fb70
Haseeb <haseebpvt@gmail.com>
2025-09-10 18:48:43 +0530

    Add shopping list route to the FastAPI application and remove old endpoint.

--- app.py
+++ app.py
@@ -11,11 +11,13 @@ from extractor.graph.builder import build_graph as build_extractor_graph
 from server.model.api_response import ApiResponse
 from server.route.product_recommendation_route import router as product_recommendation_route
 from server.route.preferences_route import router as preference_route
+from server.route.shopping_list_route import router as shopping_list_route
 
 app = FastAPI()
 
 app.include_router(product_recommendation_route, prefix="/recommend", tags=["Recommend"])
 app.include_router(preference_route, prefix="/preference", tags=["Preference"])
+app.include_router(shopping_list_route, prefix="/shopping_list", tags=["ShoppingList"])
 
 
 @app.get("/")
@@ -47,29 +49,6 @@ async def insert_shopping_list_and_preferences(
     return ApiResponse(
         success=True
     )
-
-
-@app.get("/get_shopping_list")
-async def get_shopping_list(
-        database_service: Annotated[DatabaseService, Depends(get_database_service)],
-        user_id: str = Form(...),
-):
-    result = database_service.get_shopping_list(user_id=user_id)
-
-    final_data = list(map(lambda item: _process_result(item), result))
-
-    return ApiResponse(
-        success=True,
-        data=final_data
-    )
-
-
-def _process_result(data):
-    shopping_list, category = data
-    shopping_list_json = json.loads(shopping_list.model_dump_json())
-    category_json = {"category_name": category.name}
-
-    return shopping_list_json | category_json
 
 
 if __name__ == "__main__":
