36fcee549372445353758caf5bd177478b7bd2d3
haseebpvt <haseebpvt@gmail.com>
2025-09-10 21:18:44 +0530

    Add endpoint and service to mark a shopping list item

--- server/route/shopping_list_route.py
+++ server/route/shopping_list_route.py
@@ -25,6 +25,19 @@ async def get_shopping_list(
     )
 
 
+@router.post("/mark_purchased")
+async def mark_purchased(
+        database_service: Annotated[DatabaseService, Depends(get_database_service)],
+        item_id: int = Form(...),
+        is_purchased: bool  = Form(...),
+):
+    database_service.mark_product_purchased(item_id=item_id, is_purchased=is_purchased)
+
+    return ApiResponse(
+        success=True,
+    )
+
+
 def _process_shopping_list_result(data):
     shopping_list, category = data
     shopping_list_json = json.loads(shopping_list.model_dump_json())
