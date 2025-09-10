64bbe1329c84385f14893e98e1b4257864f049ad
Haseeb <haseebpvt@gmail.com>
2025-09-10 18:51:18 +0530

    Refactor app.py to modularize extractor functionality into a new route.

--- app.py
+++ app.py
@@ -1,54 +1,22 @@
-import json
-from typing import Annotated
-
 import uvicorn
-from fastapi import FastAPI, Form, Depends
-from pytidb import Table
+from fastapi import FastAPI
 
-from db.database_service import DatabaseService
-from di.dependencies import get_preference_table, get_shopping_list_table, get_database_service
-from extractor.graph.builder import build_graph as build_extractor_graph
-from server.model.api_response import ApiResponse
-from server.route.product_recommendation_route import router as product_recommendation_route
 from server.route.preferences_route import router as preference_route
+from server.route.product_recommendation_route import router as product_recommendation_route
 from server.route.shopping_list_route import router as shopping_list_route
+from server.route.extractor_route import router as extractor_route
 
 app = FastAPI()
 
 app.include_router(product_recommendation_route, prefix="/recommend", tags=["Recommend"])
 app.include_router(preference_route, prefix="/preference", tags=["Preference"])
 app.include_router(shopping_list_route, prefix="/shopping_list", tags=["ShoppingList"])
+app.include_router(extractor_route, prefix="/extractor", tags=["Extractor"])
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World x"}
-
-
-@app.post("/insert_data", response_model=ApiResponse)
-async def insert_shopping_list_and_preferences(
-        preference_table: Annotated[Table, Depends(get_preference_table)],
-        shopping_list_table: Annotated[Table, Depends(get_shopping_list_table)],
-        user_id: str = Form(...),
-        user_text: str = Form(...),
-):
-    graph = build_extractor_graph()
-
-    config = {
-        "configurable": {
-            "preference_table": preference_table,
-            "shopping_list_table": shopping_list_table
-        }
-    }
-
-    result = await graph.ainvoke(
-        input={"user_id": user_id, "user_text": user_text},
-        config=config,
-    )
-
-    return ApiResponse(
-        success=True
-    )
 
 
 if __name__ == "__main__":
