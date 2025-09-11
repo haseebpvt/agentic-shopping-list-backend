0118c4cf3ba12f5eeb8a4261f9973038352dccf8
haseebpvt <haseebpvt@gmail.com>
2025-09-11 12:04:20 +0530

    Add category_table dependency to shopping list and preferences endpoint

--- server/route/extractor_route.py
+++ server/route/extractor_route.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import Form, Depends, APIRouter
 from pytidb import Table
 
-from di.dependencies import get_preference_table, get_shopping_list_table
+from di.dependencies import get_preference_table, get_shopping_list_table, get_category_table
 from extractor.graph.builder import build_graph as build_extractor_graph
 from server.model.api_response import ApiResponse
 
@@ -14,6 +14,7 @@ router = APIRouter()
 async def insert_shopping_list_and_preferences(
         preference_table: Annotated[Table, Depends(get_preference_table)],
         shopping_list_table: Annotated[Table, Depends(get_shopping_list_table)],
+        category_table: Annotated[Table, Depends(get_category_table)],
         user_id: str = Form(...),
         user_text: str = Form(...),
 ):
@@ -22,7 +23,8 @@ async def insert_shopping_list_and_preferences(
     config = {
         "configurable": {
             "preference_table": preference_table,
-            "shopping_list_table": shopping_list_table
+            "shopping_list_table": shopping_list_table,
+            "category": category_table,
         }
     }
 
