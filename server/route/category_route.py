0091e1a001af3f6e5010829dae88b7bb3f916b47
Haseeb <haseebpvt@gmail.com>
2025-09-12 12:26:03 +0530

    Add category data models and implement prompt generation for categories.

--- server/route/category_route.py
+++ server/route/category_route.py
@@ -1,11 +1,24 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import APIRouter, Depends, Form
+from pydantic import BaseModel
 
 from db.database_service import DatabaseService
 from di.dependencies import get_database_service
+from llm.llm import get_llm
+from prompt.prompt_loader import get_prompt_template
+from server.model.api_response import ApiResponse
 
 router = APIRouter()
+
+
+class Category(BaseModel):
+    id: int
+    name: str
+
+
+class CategoryList(BaseModel):
+    categories: List[Category]
 
 
 @router.get("/categories")
@@ -22,4 +35,20 @@ async def identify_category(
         database_service: Annotated[DatabaseService, Depends(get_database_service)],
         item_name: str = Form(...),
 ):
+    # Get category list from db
     categories = database_service.get_categories()
+
+    # Generate prompt
+    formatted_category_list = list(map(lambda i: f"{i.name} - ID: {i.id}", categories))
+    prompt = get_prompt_template(
+        "identify_category",
+        **{"categories": formatted_category_list, "item_name": item_name},
+    )
+
+    llm = get_llm()
+    result = llm.with_structured_output(CategoryList).invoke(prompt)
+
+    return ApiResponse(
+        success=True,
+        data=result,
+    )
